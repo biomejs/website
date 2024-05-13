8eafad76d1684176aaaa6e1252dfb6a879c008f9
Emanuele Stoppa <my.burning@gmail.com>
2024-05-13 11:05:42 +0100

    try different approach

--- src/pages/metadata/metadata.json.js
+++ src/pages/metadata/metadata.json.js
@@ -2,7 +2,7 @@ import { readFileSync } from "node:fs";
 import { resolve } from "node:path";
 
 export function GET() {
-	const root = resolve(import.meta.dirname, "_metadata.json");
+	const root = resolve("..", "metadata", "_metadata.json");
 	// const json_file = new URL("_metadata.json", root);
 	const schema = readFileSync(root, "utf8");
 	return new Response(schema, {
