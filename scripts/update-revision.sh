8fcf89d4ff40ac8dfed7617537d7dc4d400bd2c3
Ze-Zheng Wu <zezhengwu@proton.me>
2024-06-07 20:18:33 +0800

    chore(ci): fix comment (#532)

--- scripts/update-revision.sh
+++ scripts/update-revision.sh
@@ -14,7 +14,7 @@ new_rev=$1
 # Update the @biomejs/wasm-web dependency
 pnpm add -D https://pkg.pr.new/biomejs/biome/@biomejs/wasm-web@$(echo $new_rev | cut -c 1-7)
 
-# Update the rev values for the biome dependencies (uses GNU sed!!!)
+# Update the rev values for the biome dependencies
 awk -v new_rev="$new_rev" '{gsub(/rev = "[^"]+"/, "rev = \"" new_rev "\""); print}' Cargo.toml >temp.toml && mv temp.toml Cargo.toml
 
 echo "Revision updated to $new_rev."
