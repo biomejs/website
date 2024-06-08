b91d9d1e6fd33ed9845347952636699fa9b06d0f
Ze-Zheng Wu <zezhengwu@proton.me>
2024-06-08 17:32:38 +0800

    feat(playground): display Biome and Prettier version

--- src/playground/components/BiomeHeader.tsx
+++ src/playground/components/BiomeHeader.tsx
@@ -1,10 +1,35 @@
 import biomeIcon from "@/assets/svg/logomark.svg";
+import { type MouseEvent, useCallback } from "react";
+
+const longCommitHash = BIOME_VERSION.slice(10);
+const shortCommitHash = longCommitHash.slice(0, 7);
+
+function BiomeVersion() {
+	const link = `https://github.com/biomejs/biome/tree/${longCommitHash}`;
+
+	const handleClick = useCallback((e: MouseEvent<HTMLAnchorElement>) => {
+		e.stopPropagation();
+	}, []);
+
+	return (
+		<a
+			className="formatter-version"
+			href={link}
+			title="commit hash"
+			onClick={handleClick}
+		>
+			{shortCommitHash}
+		</a>
+	);
+}
 
 export default function BiomeHeader() {
 	return (
 		<>
 			<img alt="Biome logo" src={biomeIcon.src} />
-			<span>Biome</span>
+			<span>
+				Biome <BiomeVersion />
+			</span>
 		</>
 	);
 }
