8e36a53a0d95318c2b0652141d47e90c34e644e4
Ze-Zheng Wu <zezhengwu@proton.me>
2024-06-20 19:29:38 +0800

    test

--- src/playground/main.tsx
+++ src/playground/main.tsx
@@ -5,6 +5,9 @@ import { createRoot } from "react-dom/client";
 const container = document.getElementById("root");
 const root = createRoot(container!);
 
+let b;
+debugger;
+
 root.render(
 	<React.StrictMode>
 		<PlaygroundLoader />
