4936985d2d6b94b1b9b2ac3f3a0459a5e437f9ea
Nicolas Hedger <649677+nhedger@users.noreply.github.com>
2024-09-15 11:57:56 +0200

    fix: include inline code in rule description (#1122)

--- codegen/src/lintdoc.rs
+++ codegen/src/lintdoc.rs
@@ -1134,6 +1134,7 @@ fn events_to_text(events: Vec<Event>) -> String {
     for event in events {
         match event {
             Event::Text(text) => buffer.push_str(&*text),
+            Event::Code(text) => buffer.push_str(format!("`{}`", text).as_str()),
             _ => {}
         }
     }
