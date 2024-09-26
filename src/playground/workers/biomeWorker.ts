ed1804ed01ea75463dca5fe671c7c5ef82eb2f71
chansuke <moonset20@gmail.com>
2024-09-26 14:59:34 +0900

    fix: `biome check` error

--- src/playground/workers/biomeWorker.ts
+++ src/playground/workers/biomeWorker.ts
@@ -1,31 +1,31 @@
 import {
-    ArrowParentheses,
-    AttributePosition,
-    type BiomeOutput,
-    IndentStyle,
-    LintRules,
-    LoadingState,
-    type PlaygroundSettings,
-    QuoteProperties,
-    QuoteStyle,
-    Semicolons,
+	ArrowParentheses,
+	AttributePosition,
+	type BiomeOutput,
+	IndentStyle,
+	LintRules,
+	LoadingState,
+	type PlaygroundSettings,
+	QuoteProperties,
+	QuoteStyle,
+	Semicolons,
 } from "@/playground/types";
 import init, {
-    DiagnosticPrinter,
-    type PartialConfiguration as Configuration,
-    type BiomePath,
-    type RuleCategories,
-    Workspace,
+	DiagnosticPrinter,
+	type PartialConfiguration as Configuration,
+	type BiomePath,
+	type RuleCategories,
+	Workspace,
 } from "@biomejs/wasm-web";
 
 let workspace: Workspace | null = null;
 let fileCounter = 0;
 
 type File = {
-    filename: string;
-    id: number;
-    content: string;
-    version: number;
+	filename: string;
+	id: number;
+	content: string;
+	version: number;
 };
 
 const files: Map<string, File> = new Map();
@@ -33,305 +33,303 @@ const files: Map<string, File> = new Map();
 let configuration: undefined | Configuration;
 
 function getPathForFile(file: File): BiomePath {
-    return {
-        path: file.filename,
-        kind: ["Handleable"],
-        was_written: false,
-    };
+	return {
+		path: file.filename,
+		kind: ["Handleable"],
+		was_written: false,
+	};
 }
 
 self.addEventListener("message", async (e) => {
-    switch (e.data.type) {
-        case "init": {
-            try {
-                if (import.meta.env.DEV) {
-                    await init(
-                        "../../../node_modules/@biomejs/wasm-web/biome_wasm_bg.wasm?init",
-                    );
-                } else {
-                    await init();
-                }
+	switch (e.data.type) {
+		case "init": {
+			try {
+				if (import.meta.env.DEV) {
+					await init(
+						"../../../node_modules/@biomejs/wasm-web/biome_wasm_bg.wasm?init",
+					);
+				} else {
+					await init();
+				}
 
-                workspace = new Workspace();
-                workspace.registerProjectFolder({
-                    setAsCurrentWorkspace: true,
-                });
+				workspace = new Workspace();
+				workspace.registerProjectFolder({
+					setAsCurrentWorkspace: true,
+				});
 
-                self.postMessage({type: "init", loadingState: LoadingState.Success});
-            } catch (err) {
-                console.error(err);
-                self.postMessage({type: "init", loadingState: LoadingState.Error});
-            }
+				self.postMessage({ type: "init", loadingState: LoadingState.Success });
+			} catch (err) {
+				console.error(err);
+				self.postMessage({ type: "init", loadingState: LoadingState.Error });
+			}
 
-            break;
-        }
+			break;
+		}
 
-        case "updateSettings": {
-            if (!workspace) {
-                console.error("Workspace was not initialized");
-                break;
-            }
+		case "updateSettings": {
+			if (!workspace) {
+				console.error("Workspace was not initialized");
+				break;
+			}
 
-            const {
-                lineWidth,
-                indentStyle,
-                indentWidth,
-                quoteStyle,
-                jsxQuoteStyle,
-                quoteProperties,
-                lintRules,
-                enabledLinting,
-                trailingCommas,
-                semicolons,
-                arrowParentheses,
-                bracketSpacing,
-                bracketSameLine,
-                importSortingEnabled,
-                unsafeParameterDecoratorsEnabled,
-                allowComments,
-                attributePosition,
-            } = e.data.settings as PlaygroundSettings;
+			const {
+				lineWidth,
+				indentStyle,
+				indentWidth,
+				quoteStyle,
+				jsxQuoteStyle,
+				quoteProperties,
+				lintRules,
+				enabledLinting,
+				trailingCommas,
+				semicolons,
+				arrowParentheses,
+				bracketSpacing,
+				bracketSameLine,
+				importSortingEnabled,
+				unsafeParameterDecoratorsEnabled,
+				allowComments,
+				attributePosition,
+			} = e.data.settings as PlaygroundSettings;
 
-            configuration = {
-                formatter: {
-                    enabled: true,
-                    formatWithErrors: true,
-                    lineWidth: lineWidth,
-                    indentStyle: indentStyle === IndentStyle.Tab ? "tab" : "space",
-                    indentWidth,
-                    attributePosition:
-                        attributePosition === AttributePosition.Auto ? "auto" : "multiline",
-                },
+			configuration = {
+				formatter: {
+					enabled: true,
+					formatWithErrors: true,
+					lineWidth: lineWidth,
+					indentStyle: indentStyle === IndentStyle.Tab ? "tab" : "space",
+					indentWidth,
+					attributePosition:
+						attributePosition === AttributePosition.Auto ? "auto" : "multiline",
+				},
 
-                linter: {
-                    enabled: enabledLinting,
-                },
+				linter: {
+					enabled: enabledLinting,
+				},
 
-                organizeImports: {
-                    enabled: importSortingEnabled,
-                },
+				organizeImports: {
+					enabled: importSortingEnabled,
+				},
 
-                javascript: {
-                    formatter: {
-                        quoteStyle: quoteStyle === QuoteStyle.Double ? "double" : "single",
-                        jsxQuoteStyle:
-                            jsxQuoteStyle === QuoteStyle.Double ? "double" : "single",
-                        quoteProperties:
-                            quoteProperties === QuoteProperties.Preserve
-                                ? "preserve"
-                                : "asNeeded",
-                        trailingCommas,
-                        semicolons:
-                            semicolons === Semicolons.Always ? "always" : "asNeeded",
-                        arrowParentheses:
-                            arrowParentheses === ArrowParentheses.Always
-                                ? "always"
-                                : "asNeeded",
-                        bracketSpacing,
-                        bracketSameLine,
-                        attributePosition:
-                            attributePosition === AttributePosition.Auto
-                                ? "auto"
-                                : "multiline",
-                    },
-                    parser: {
-                        unsafeParameterDecoratorsEnabled,
-                    },
-                },
-                css: {
-                    formatter: {
-                        quoteStyle: quoteStyle === QuoteStyle.Double ? "double" : "single",
-                    },
-                    parser: {
-                        allowWrongLineComments: true,
-                    },
-                },
-                json: {
-                    parser: {
-                        allowComments,
-                    },
-                },
-            };
+				javascript: {
+					formatter: {
+						quoteStyle: quoteStyle === QuoteStyle.Double ? "double" : "single",
+						jsxQuoteStyle:
+							jsxQuoteStyle === QuoteStyle.Double ? "double" : "single",
+						quoteProperties:
+							quoteProperties === QuoteProperties.Preserve
+								? "preserve"
+								: "asNeeded",
+						trailingCommas,
+						semicolons:
+							semicolons === Semicolons.Always ? "always" : "asNeeded",
+						arrowParentheses:
+							arrowParentheses === ArrowParentheses.Always
+								? "always"
+								: "asNeeded",
+						bracketSpacing,
+						bracketSameLine,
+						attributePosition:
+							attributePosition === AttributePosition.Auto
+								? "auto"
+								: "multiline",
+					},
+					parser: {
+						unsafeParameterDecoratorsEnabled,
+					},
+				},
+				css: {
+					formatter: {
+						quoteStyle: quoteStyle === QuoteStyle.Double ? "double" : "single",
+					},
+					parser: {
+						allowWrongLineComments: true,
+					},
+				},
+				json: {
+					parser: {
+						allowComments,
+					},
+				},
+			};
 
-            switch (lintRules) {
-                case LintRules.Recommended: {
-                    configuration.linter!.rules = {
-                        nursery: {
-                            recommended: false,
-                        },
-                    };
-                    break;
-                }
-                case LintRules.All: {
-                    configuration.linter!.rules = {
-                        all: true,
-                    };
-                    break;
-                }
-            }
+			switch (lintRules) {
+				case LintRules.Recommended: {
+					configuration.linter!.rules = {
+						nursery: {
+							recommended: false,
+						},
+					};
+					break;
+				}
+				case LintRules.All: {
+					configuration.linter!.rules = {
+						all: true,
+					};
+					break;
+				}
+			}
 
-            workspace.updateSettings({
-                configuration,
-                gitignore_matches: [],
-            });
-            break;
-        }
+			workspace.updateSettings({
+				configuration,
+				gitignore_matches: [],
+			});
+			break;
+		}
 
-        case "update": {
-            if (!workspace) {
-                console.error("Workspace was not initialized");
-                break;
-            }
+		case "update": {
+			if (!workspace) {
+				console.error("Workspace was not initialized");
+				break;
+			}
 
-            const {filename, code, cursorPosition} = e.data;
+			const { filename, code, cursorPosition } = e.data;
 
-            let file = files.get(filename);
-            if (file === undefined) {
-                file = {
-                    filename,
-                    version: 0,
-                    content: code,
-                    id: fileCounter++,
-                };
+			let file = files.get(filename);
+			if (file === undefined) {
+				file = {
+					filename,
+					version: 0,
+					content: code,
+					id: fileCounter++,
+				};
 
-                workspace.openFile({
-                    path: getPathForFile(file),
-                    version: 0,
-                    content: code,
-                });
-            } else {
-                file = {
-                    filename,
-                    id: file.id,
-                    version: file.version + 1,
-                    content: code,
-                };
+				workspace.openFile({
+					path: getPathForFile(file),
+					version: 0,
+					content: code,
+				});
+			} else {
+				file = {
+					filename,
+					id: file.id,
+					version: file.version + 1,
+					content: code,
+				};
 
-                workspace.openFile({
-                    path: getPathForFile(file),
-                    version: file.version,
-                    content: code,
-                });
-            }
-            files.set(filename, file);
-            const path = getPathForFile(file);
-            const fileFeatures = workspace.fileFeatures({
-                features: ["Debug", "Format", "Lint", "OrganizeImports"],
-                path,
-            });
+				workspace.openFile({
+					path: getPathForFile(file),
+					version: file.version,
+					content: code,
+				});
+			}
+			files.set(filename, file);
+			const path = getPathForFile(file);
+			const fileFeatures = workspace.fileFeatures({
+				features: ["Debug", "Format", "Lint", "OrganizeImports"],
+				path,
+			});
 
-            const syntaxTree =
-                fileFeatures.features_supported.get("Debug") === "Supported"
-                    ? workspace.getSyntaxTree({
-                        path,
-                    })
-                    : {ast: "Not supported", cst: "Not supported"};
+			const syntaxTree =
+				fileFeatures.features_supported.get("Debug") === "Supported"
+					? workspace.getSyntaxTree({
+							path,
+						})
+					: { ast: "Not supported", cst: "Not supported" };
 
-            let controlFlowGraph = "";
-            try {
-                controlFlowGraph =
-                    fileFeatures.features_supported.get("Debug") === "Supported"
-                        ? workspace.getControlFlowGraph({
-                            path,
-                            cursor: cursorPosition,
-                        })
-                        : "";
-            } catch (e) {
-                console.warn("Failed to get control flow graph:", e);
-                controlFlowGraph = "";
-            }
+			let controlFlowGraph = "";
+			try {
+				controlFlowGraph =
+					fileFeatures.features_supported.get("Debug") === "Supported"
+						? workspace.getControlFlowGraph({
+								path,
+								cursor: cursorPosition,
+							})
+						: "";
+			} catch (e) {
+				console.warn("Failed to get control flow graph:", e);
+				controlFlowGraph = "";
+			}
 
-            let formatterIr;
-            try {
+			let formatterIr = "";
+			try {
+				fileFeatures.features_supported.get("Debug") === "Supported"
+					? workspace.getFormatterIr({
+							path,
+						})
+					: "Not supported";
+			} catch (e) {
+				console.error(e);
+				formatterIr = "Can't format";
+			}
 
-                fileFeatures.features_supported.get("Debug") === "Supported"
-                    ? workspace.getFormatterIr({
-                        path,
-                    })
-                    : "Not supported";
-            } catch (e) {
-                console.error(e);
-                formatterIr = "Can't format";
-            }
+			const importSorting =
+				fileFeatures.features_supported.get("OrganizeImports") === "Supported"
+					? workspace.organizeImports({
+							path,
+						})
+					: {
+							code:
+								fileFeatures.features_supported.get("OrganizeImports") ??
+								"Not supported",
+						};
 
-            const importSorting =
-                fileFeatures.features_supported.get("OrganizeImports") === "Supported"
-                    ? workspace.organizeImports({
-                        path,
-                    })
-                    : {
-                        code:
-                            fileFeatures.features_supported.get("OrganizeImports") ??
-                            "Not supported",
-                    };
+			const categories: RuleCategories = [];
+			if (configuration?.formatter?.enabled) {
+				categories.push("Syntax");
+			}
+			if (configuration?.linter?.enabled) {
+				categories.push("Lint");
+			}
+			const diagnosticsResult = workspace.pullDiagnostics({
+				path,
+				categories: categories,
+				max_diagnostics: Number.MAX_SAFE_INTEGER,
+				only: [],
+				skip: [],
+			});
 
-            const categories: RuleCategories = [];
-            if (configuration?.formatter?.enabled) {
-                categories.push("Syntax");
-            }
-            if (configuration?.linter?.enabled) {
-                categories.push("Lint");
-            }
-            const diagnosticsResult = workspace.pullDiagnostics({
-                path,
-                categories: categories,
-                max_diagnostics: Number.MAX_SAFE_INTEGER,
-                only: [],
-                skip: [],
-            });
+			const printer = new DiagnosticPrinter(path.path, code);
+			for (const diag of diagnosticsResult.diagnostics) {
+				printer.print_verbose(diag);
+			}
 
-            const printer = new DiagnosticPrinter(path.path, code);
-            for (const diag of diagnosticsResult.diagnostics) {
-                printer.print_verbose(diag);
-            }
+			let printed = "";
+			try {
+				fileFeatures.features_supported.get("Format") === "Supported"
+					? workspace.formatFile({
+							path,
+						})
+					: { code: "Not supported" };
+			} catch (e) {
+				console.error(e);
+				printed = {
+					code: "Can't format with errors",
+				};
+			}
 
-            let printed;
-            try {
-                fileFeatures.features_supported.get("Format") === "Supported"
-                    ? workspace.formatFile({
-                        path,
-                    })
-                    : {code: "Not supported"};
-            } catch (e) {
-                console.error(e)
-                printed = {
-                    code: "Can't format with errors"
-                }
-            }
+			const biomeOutput: BiomeOutput = {
+				syntax: {
+					// Replace 4 spaced indentation with 2
+					// TODO replace this in Biome itself
+					ast: syntaxTree.ast.replace(/ {4}/g, "  "),
+					cst: syntaxTree.cst,
+				},
+				diagnostics: {
+					console: printer.finish(),
+					list: diagnosticsResult.diagnostics,
+				},
+				formatter: {
+					code: printed!.code,
+					ir: formatterIr!,
+				},
+				analysis: {
+					controlFlowGraph,
+				},
+				importSorting: {
+					code: importSorting.code,
+				},
+			};
 
+			self.postMessage({
+				type: "updated",
+				filename,
+				biomeOutput,
+			});
+			break;
+		}
 
-            const biomeOutput: BiomeOutput = {
-                syntax: {
-                    // Replace 4 spaced indentation with 2
-                    // TODO replace this in Biome itself
-                    ast: syntaxTree.ast.replace(/ {4}/g, "  "),
-                    cst: syntaxTree.cst,
-                },
-                diagnostics: {
-                    console: printer.finish(),
-                    list: diagnosticsResult.diagnostics,
-                },
-                formatter: {
-                    code: printed!.code,
-                    ir: formatterIr!,
-                },
-                analysis: {
-                    controlFlowGraph,
-                },
-                importSorting: {
-                    code: importSorting.code,
-                },
-            };
-
-            self.postMessage({
-                type: "updated",
-                filename,
-                biomeOutput,
-            });
-            break;
-        }
-
-        default:
-            console.error(`Unknown message '${e.data.type}'.`);
-    }
+		default:
+			console.error(`Unknown message '${e.data.type}'.`);
+	}
 });
