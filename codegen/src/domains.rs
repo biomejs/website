6019dc21a9744307a018d1203fdb0aeeeb0b3e02
Emanuele Stoppa <my.burning@gmail.com>
2025-04-22 08:54:56 +0100

    chore: update lint domains

--- codegen/src/domains.rs
+++ codegen/src/domains.rs
@@ -91,6 +91,13 @@ impl DocDomains {
                     writeln!(buffer, "## {name}")?;
                     writeln!(buffer, "Use this domain inside Next.js projects.")?;
                 }
+                RuleDomain::Project => {
+                    writeln!(buffer, "## {name}")?;
+                    writeln!(
+                        buffer,
+                        "This domain enables rules that require scanning the entire project to work. When enabling rules that belong to this domain, Biome will scan the entire project. The scanning phase will have a performance impact on the linting process."
+                    )?;
+                }
             }
             Self::write_activation(name.as_str(), buffer)?;
             Self::write_dependencies(domain, name.as_str(), buffer)?;
