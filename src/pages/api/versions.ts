a3181179d0ec06a40229f041912d25daa0afe253
Nicolas Hedger <nicolas@hedger.ch>
2025-05-29 21:09:52 +0200

    feat(api): add version endpoints

---
+++ src/pages/api/versions.ts
@@ -0,0 +1,24 @@
+import { getAllVersions } from "@biomejs/version-utils";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ request }) => {
+	const url = new URL(request.url);
+
+	const includePrereleases = url.searchParams.get("prereleases") === "true";
+	const wantedFormat = request.headers.get("Accept") || "application/json";
+	const versions = (await getAllVersions(includePrereleases)) ?? [];
+
+	if (wantedFormat === "text/plain") {
+		return new Response(versions.join("\n"), {
+			headers: {
+				"Content-Type": "text/plain",
+			},
+		});
+	}
+
+	return new Response(JSON.stringify(versions), {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+};
---
+++ src/pages/api/versions/latest.ts
@@ -0,0 +1,24 @@
+import { getLatestVersion } from "@biomejs/version-utils";
+import type { APIRoute } from "astro";
+
+export const GET: APIRoute = async ({ request }) => {
+	const url = new URL(request.url);
+
+	const prerelease = url.searchParams.get("prerelease") === "true";
+	const wantedFormat = request.headers.get("Accept") || "application/json";
+	const version = await getLatestVersion(prerelease ? "nightly" : "stable");
+
+	if (wantedFormat === "text/plain") {
+		return new Response(version, {
+			headers: {
+				"Content-Type": "text/plain",
+			},
+		});
+	}
+
+	return new Response(JSON.stringify({ version: version }), {
+		headers: {
+			"Content-Type": "application/json",
+		},
+	});
+};
