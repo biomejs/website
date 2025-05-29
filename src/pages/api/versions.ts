c298a156351cd3bea3eab4813e79ed23f781aefd
Nicolas Hedger <nicolas@hedger.ch>
2025-05-29 21:24:45 +0200

    disable pre-rendering

--- src/pages/api/versions.ts
+++ src/pages/api/versions.ts
@@ -1,6 +1,8 @@
 import { getAllVersions } from "@biomejs/version-utils";
 import type { APIRoute } from "astro";
 
+export const prerender = false;
+
 export const GET: APIRoute = async ({ request }) => {
 	const url = new URL(request.url);
 
