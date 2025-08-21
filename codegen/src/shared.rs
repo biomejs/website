30939f519eeb164dcdce2e51466d93f72cd6db69
Maikel <maikel.van.dort@gmail.com>
2025-08-21 15:49:52 +0200

    fix: feedback

--- codegen/src/shared.rs
+++ codegen/src/shared.rs
@@ -19,7 +19,7 @@ pub fn add_codegen_rule_suggestion(content: &mut Vec<u8>) -> anyhow::Result<()> 
     writeln!(content)?;
     writeln!(
         content,
-        "Missing a rule? Help us by [contributing](https://github.com/biomejs/biome/blob/main/CONTRIBUTING.md) or create a rule suggestion [here](https://github.com/biomejs/biome/discussions/categories/rule-suggestion)."
+        "Missing a rule? Help us by [contributing](https://github.com/biomejs/biome/blob/main/CONTRIBUTING.md) to the [analyzer](https://github.com/biomejs/biome/blob/main/crates/biome_analyze/CONTRIBUTING.md) or create a rule suggestion [here](https://github.com/biomejs/biome/discussions/categories/rule-suggestion)."
     )?;
     Ok(())
 }
