81c42fde769cc57b8398768739533e530ca8a904
Emanuele Stoppa <my.burning@gmail.com>
2025-08-29 09:59:25 +0100

    feat: use plugin for better changelog

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,6 +3,7 @@ import { docsLoader, i18nLoader } from "@astrojs/starlight/loaders";
 import { docsSchema, i18nSchema } from "@astrojs/starlight/schema";
 import { file } from "astro/loaders";
 import { blogSchema } from "starlight-blog/schema";
+import { changelogsLoader } from "starlight-changelogs/loader";
 
 export const collections = {
 	docs: defineCollection({
@@ -18,6 +19,25 @@ export const collections = {
 				type: z.enum(["core", "maintainer"]),
 			}),
 		loader: file("src/content/team.json"),
+	}),
+	changelogs: defineCollection({
+		loader: changelogsLoader([
+			{
+				base: "internals/changelog",
+				provider: "changeset",
+				title: {
+					en: "Version History",
+					es: "Registro de cambios",
+					ja: "変更履歴",
+					"zh-CN": "更新日志",
+					"pt-BR": "Alterações",
+					uk: "Журнал змін",
+					ru: "Журнал изменений",
+				},
+				changelog:
+					"https://raw.githubusercontent.com/biomejs/biome/refs/heads/main/packages/%40biomejs/biome/CHANGELOG.md",
+			},
+		]),
 	}),
 	i18n: defineCollection({
 		loader: i18nLoader(),
