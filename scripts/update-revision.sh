8ba5054781ba7f6b20483d63f3cf0805ee587359
Nicolas Hedger <nicolas@hedger.ch>
2026-03-15 20:10:06 +0100

    remove uneccessary step

--- scripts/update-revision.sh
+++ scripts/update-revision.sh
@@ -18,9 +18,6 @@ jq ".devDependencies[\"@biomejs/wasm-web\"] = \"https://pkg.pr.new/biomejs/biome
 # Install dependencies
 pnpm install --no-frozen-lockfile
 
-# Update the rev values for the biome dependencies
-awk -v new_rev="$new_rev" '{gsub(/rev = "[^"]+"/, "rev = \"" new_rev "\""); print}' Cargo.toml >temp.toml && mv temp.toml Cargo.toml
-
 # Identify all top-level biome crates
 biome_crates=$(
   cargo tree --prefix none --depth 1 --format '{p}' \
