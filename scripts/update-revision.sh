366e96a577550354b6bbe33dda5069a2fac05aca
Nicolas Hedger <nicolas@hedger.ch>
2026-03-15 18:36:03 +0100

    ci: fix revision update script

--- scripts/update-revision.sh
+++ scripts/update-revision.sh
@@ -22,6 +22,6 @@ pnpm install --no-frozen-lockfile
 awk -v new_rev="$new_rev" '{gsub(/rev = "[^"]+"/, "rev = \"" new_rev "\""); print}' Cargo.toml >temp.toml && mv temp.toml Cargo.toml
 
 # Update Cargo.lock
-cargo update --git https://github.com/biomejs/biome.git
+cargo update
 
 echo "Revision updated to $new_rev."
