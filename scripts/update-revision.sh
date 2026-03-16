ac07cc176e85c1ce8c68b87f342957a1f34f7ed4
Nicolas Hedger <649677+nhedger@users.noreply.github.com>
2026-03-16 07:47:56 +0100

    ci: fix update revision script (#4063)

--- scripts/update-revision.sh
+++ scripts/update-revision.sh
@@ -18,10 +18,21 @@ jq ".devDependencies[\"@biomejs/wasm-web\"] = \"https://pkg.pr.new/biomejs/biome
 # Install dependencies
 pnpm install --no-frozen-lockfile
 
-# Update the rev values for the biome dependencies
-awk -v new_rev="$new_rev" '{gsub(/rev = "[^"]+"/, "rev = \"" new_rev "\""); print}' Cargo.toml >temp.toml && mv temp.toml Cargo.toml
+# Identify all top-level biome crates
+biome_crates=$(
+  cargo tree --prefix none --depth 1 --format '{p}' \
+    | grep '^biome_' \
+    | awk '{print $1}'
+)
 
-# Update Cargo.lock
-cargo update
+# Update each biome crate to point to the new revision
+if [ -n "$biome_crates" ]; then
+  # shellcheck disable=SC2086
+  set -- $biome_crates
+  cargo add \
+    --git https://github.com/biomejs/biome.git \
+    --rev "$new_rev" \
+    "$@"
+fi
 
 echo "Revision updated to $new_rev."
