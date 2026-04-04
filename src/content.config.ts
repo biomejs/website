a226084aed58b45da6709794ecaa7c1c4ca56eca
Carson McManus <carson.mcmanus1@gmail.com>
2026-04-04 18:24:12 -0400

    fix: exclude changelogs from search

--- src/content.config.ts
+++ src/content.config.ts
@@ -35,6 +35,7 @@ export const collections = {
 		loader: changelogsLoader([
 			{
 				base: "internals/changelog",
+				pagefind: false,
 				provider: "changeset",
 				title: {
 					en: "Version History",
